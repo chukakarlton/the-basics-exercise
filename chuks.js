164aed8fa80083794f4480ed4d9e826a047a6534
chukakarl <chukakarl1@gmail.com>
2024-04-23 12:31:34 +0100

    .

---
+++ chuks.js
@@ -0,0 +1,9 @@
+// Define variables for first name and last name
+let firstName = "chuka";
+let lastName = "carlton";
+
+// Concatenate the first name and last name with a space in between
+let fullName = firstName + lastName;
+
+// Output the full name
+console.log(fullName);
